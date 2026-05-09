f19efed91c2369c0ccd7b87afe27edfd8823b93c
Minhyeok Choi <bbbim5656@gmail.com>
2026-05-09 12:02:31 +0900

    람다 학습 3-1

---
+++ src/lambda/lambda4/SupplierMain.java
@@ -0,0 +1,22 @@
+package lambda.lambda4;
+
+import java.util.Random;
+import java.util.function.Supplier;
+
+public class SupplierMain {
+
+    static void main(String[] args) {
+        Supplier<Integer> supplier1 = new Supplier<Integer>() {
+            @Override
+            public Integer get() {
+                return new Random().nextInt(10);
+            }
+        };
+
+        System.out.println(supplier1.get());
+
+        Supplier<Integer> supplier2 = () -> new Random().nextInt(10);
+
+        System.out.println(supplier2.get());
+    }
+}
---
+++ src/lambda/lambda4/ex2/FilterExampleEx2.java
@@ -0,0 +1,24 @@
+package lambda.lambda4.ex2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class FilterExampleEx2 {
+    static void main(String[] args) {
+        List<Integer> numbers = List.of(-3, -2, -1, 1, 2, 3, 5);
+        System.out.println("원본 리스트 : " + numbers);
+        System.out.println(filter(numbers, i -> i < 0));
+        System.out.println(filter(numbers, i -> i % 2 == 0 ));
+    }
+
+    public static List<Integer> filter(List<Integer> list, Predicate<Integer> predicate) {
+        List<Integer> result = new ArrayList<>();
+        for (int val : list) {
+            if (predicate.test(val)) {
+                result.add(val);
+            }
+        }
+        return result;
+    }
+}
